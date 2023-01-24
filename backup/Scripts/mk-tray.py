e076a05d10eb2e2d69e71098ef251733824f4226
mdjames094 <mdjames094@gmail.com>
2023-01-24 02:43:39 +0100

    mk-music script

---
+++ backup/Scripts/mk-tray.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import os
+import gi
+import sys
+
+gi.require_version("Gtk", "3.0")
+gi.require_version("AppIndicator3", "0.1")
+gi.require_version('Notify', '0.7')
+from gi.repository import Gtk as gtk
+from gi.repository import AppIndicator3 as appindicator
+# from gi.repository import Notify as notify
+
+LAUNCHERS = [
+    {
+        "label": "Shuffle",
+        "icon": "/usr/share/icons/Papirus-Dark/16x16/actions/media-playlist-shuffle.svg",
+        "command": "shuffle",
+    },
+    {
+        "sep": True,
+    },
+    {
+        "label": "Exit",
+        "icon": None,
+        "command": "quit",
+    },
+]
+
+APPINDICATOR_ID = 'mk-tray'
+
+class IconoTray:
+    def __init__(self, appid, iconname):
+        self.menu = gtk.Menu()
+        self.ind = appindicator.Indicator.new(appid, iconname, appindicator.IndicatorCategory.APPLICATION_STATUS)
+        self.ind.set_status (appindicator.IndicatorStatus.ACTIVE)
+        self.ind.set_menu(self.menu)
+        self.ind.set_title('Music for work')
+        # print(dir(self.ind))
+        # notify.init(APPINDICATOR_ID)
+        # notify.Notification.new(appid, "started", None).show()
+
+    def add_menu_item(self, label=None, icon=None, command=None, sep=False):
+        if sep :
+            item = gtk.SeparatorMenuItem()
+        elif icon == None :
+            item = gtk.MenuItem()
+            item.set_label(label)
+        else :
+            img = gtk.Image()
+            img.set_from_file(icon)
+            item = gtk.ImageMenuItem(label=label)
+            item.set_image(img)
+        if command != None :
+            item.connect("activate", getattr(self, command))
+        self.menu.append(item)
+        self.menu.show_all()
+
+    def shuffle(self, source):
+        # current_label = source.get_label()
+        # os.system("nerd-dictation end")
+        self.kill()
+        os.system("mpv https://www.youtube.com/playlist?list=PLdE7uo_7KBkc6L7Bgqzz_Q7q2JN2AqHV3 --no-video --shuffle --start=$((RANDOM%100)) --length=10% --audio-display=no --force-window=no --really-quiet >/dev/null 2>&1 &")
+        return
+
+    def kill(self):
+        os.system("if [ '$(ps -o ppid= -C mpv)' ]; then ps -ef | awk '/[-]-no-video/{print $2}' | xargs kill -9 >/dev/null 2>&1; fi")
+
+    def quit(self, source):
+        self.kill()
+        gtk.main_quit()
+
+def main():
+    os.system("mpv https://www.youtube.com/playlist?list=PLdE7uo_7KBkc6L7Bgqzz_Q7q2JN2AqHV3 --no-video --shuffle --start=$((RANDOM%100)) --length=10% --audio-display=no --force-window=no --really-quiet >/dev/null 2>&1 &")
+    app = IconoTray(APPINDICATOR_ID, "google-music-manager-panel")
+    for launcher in LAUNCHERS:
+        app.add_menu_item(**launcher)
+    gtk.main()
+
+if __name__ == "__main__":
+    main()
+
+
+
--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -12,6 +12,8 @@ alias lss='du -sh */ | sort -rn'
 
 alias psa='ps -aux'
 
+alias ren="rename 's/ /_/g' * "
+
 alias update='sudo apt update'
 alias upgrade='sudo apt upgrade'
 alias distup='sudo apt dist-upgrade'
@@ -30,6 +32,7 @@ alias sysclean='sudo apt autoremove && sudo apt clean'
 alias cls='printf "\033c"'
 
 alias duse='du -sk * | sort -rn | perl -ne '\''($s,$f)=split(m{\t});for (qw(K M G)) {if($s<1024) {printf("%.1f",$s);print "$_\t$f"; last};$s=$s/1024}'\'''
+alias df='df -h | grep -v ^none | ( read header ; echo "$header" ; sort -k 1)'
 
 alias h='history | grep $2'
 
@@ -41,6 +44,7 @@ alias tka='tmux kill-session -a && tmux kill-session'
 alias ca='conda activate'
 alias cad='conda deactivate'
 alias jn='jupyter notebook'
+
 alias q='doc; cd Python/ticker; ~/anaconda3/envs/mrs/bin/python ticker_async_bash.py '
 
 alias bfc='sudo tlp fullcharge'
@@ -66,3 +70,6 @@ alias wac1="wac stylus eDP-1"
 alias wac2="wac stylus HDMI2"
 
 alias cs='function _cs { [ -z "$2" ] && curl cheat.sh/$1 || curl cheat.sh/$1/$2; unset -f _cs; }; _cs'
+
+alias mk='mpv https://www.youtube.com/playlist?list=PLdE7uo_7KBkc6L7Bgqzz_Q7q2JN2AqHV3 --no-video --shuffle --audio-display=no --force-window=no --really-quiet'
+
